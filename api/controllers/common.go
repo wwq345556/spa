9ec6ec3ef2685ca20a2e91209e6043e740634365
wwq <1175134755@qq.com>
2020-03-20 13:04:49 +0800

    【新增】接收参数处理

--- api/controllers/common.go
+++ api/controllers/common.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/validation"
 )
 
 type CommonController struct {
@@ -20,4 +22,29 @@ func (controller *CommonController) Fail(msg string) {
 	controller.Data["json"] = new(Output).ErrorOutput(msg)
 	controller.ServeJSON()
 	controller.StopRun()
+}
+
+//通过结构体获取数据
+func (controller *CommonController) GetParamStruct(paramsStruct interface{}) {
+	if err := controller.ParseForm(paramsStruct); err != nil {
+		controller.Fail(err.Error())
+	}
+	controller.CheckValidation(paramsStruct)
+}
+
+//validation验证， error直接返回
+func (controller *CommonController) CheckValidation(requestData interface{}) {
+	valid := validation.Validation{}
+	res, err := valid.Valid(requestData)
+	if err != nil {
+		controller.Fail(err.Error())
+	}
+	//包含错误信息
+	if !res {
+		errors := ""
+		for _,err := range valid.Errors {
+			errors += fmt.Sprintf("%s:%s", err.Key, err.Message)
+		}
+		controller.Fail(errors)
+	}
 }
